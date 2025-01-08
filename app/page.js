e79fd5c5ff47f72805665ff051ea3d747d787920
lucapinello <lucapinello@gmail.com>
2025-01-07 22:33:51 -0500

    fix title

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
-"use client"
-import MiniDSPController from '@/components/minidsp-controller'
+"use client";
+import MiniDSPController from '@/components/minidsp-controller';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MiniDSPController />
-    </main>
-  )
+    <>
+      <Head>
+        <title>MiniDSP Controller</title>
+        <meta name="description" content="A simple web interface to control the MiniDSP device." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <MiniDSPController />
+      </main>
+    </>
+  );
 }
